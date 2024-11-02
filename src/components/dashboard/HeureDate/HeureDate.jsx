6737ae21235783c7fbff46ecf0fe6ef32c37d5b1
Devanandhan <d.codandabany@gmail.com>
2024-11-02 15:48:30 +0100

    added another methos for HeureDate.jsx

--- src/components/dashboard/HeureDate/HeureDate.jsx
+++ src/components/dashboard/HeureDate/HeureDate.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
 const HeureDate = () => {
-  // État pour stocker la date et l'heure
   const [dateTime, setDateTime] = useState({
     date: new Date().toLocaleDateString('fr-FR', {
       weekday: 'long',
@@ -12,7 +11,6 @@ const HeureDate = () => {
     time: new Date().toLocaleTimeString('fr-FR')
   });
 
-  // Utiliser useEffect pour mettre à jour l'heure toutes les secondes
   useEffect(() => {
     const intervalId = setInterval(() => {
       const now = new Date();
@@ -25,16 +23,16 @@ const HeureDate = () => {
         }),
         time: now.toLocaleTimeString('fr-FR')
       });
-    }, 1000); // Mettre à jour chaque seconde
+    }, 1000); 
 
-    // Nettoyage de l'intervalle lors du démontage du composant
+    // 
     return () => clearInterval(intervalId);
-  }, []); // [] signifie que cet effet est appliqué une seule fois au montage
+  }, []); 
 
   return (
     <div className='useless'>
       <div className='date-color'>
-        <p>Nous somme {dateTime.date} il est {dateTime.time}</p>
+        <p>Nous sommes le {dateTime.date} il est {dateTime.time}</p>
       </div>
     </div>
   );
