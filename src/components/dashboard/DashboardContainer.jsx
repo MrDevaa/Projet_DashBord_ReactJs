bd0d4c96d7e905c74900af38f2913666262a3124
Devanandhan <d.codandabany@gmail.com>
2024-11-03 19:42:06 +0100

    markDown

--- src/components/dashboard/DashboardContainer.jsx
+++ src/components/dashboard/DashboardContainer.jsx
@@ -5,8 +5,8 @@ import Blague from './dos-blague/Blague.jsx';
 import RecetteDuJour from './dos-recette/Recette.jsx';
 import Mocktail  from'./dos-mocktail/Mocktail.jsx';
 import HistoricalDate from './dos-dateevent/DateEvent.jsx';
-import ListeMarkdowns from '../markdown/dos-listemarkdowns/ListeMarkDowns.jsx';
-import FormMarkdown from '../markdown/dos-formmarkdown/FormMarkDown.jsx';
+import ListeMarkdowns from '../MarkDown/dos-listemarkdowns/ListeMarkDowns.jsx';
+import FormMarkdown from '../MarkDown/dos-formmarkdown/FormMarkDown.jsx';
 import "../../styles/dashboard.css";
 
 function DashboardContainer({ markdowns, ajouterMarkdownViaInput, ajouterMarkdown, supprimerMarkdown }) {
